43a3bed27540daefa446f2ff5012ab731e2d22b6
Yongbo Liu <Yongbo Liu@DESKTOP-2J819AN>
2025-08-08 15:54:38 -0400

    Add project files.

---
+++ Group3_project/Products.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Group3_project
+{
+    public partial class Products : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //when login
+            if (Session["firstname"] == null || Session["lastname"] == null)
+            {
+                // back to login page
+                //string script = "alert('Please login first.'); window.location='Login.aspx';";
+                string script = "alert('Please login first.'); window.location='Login';";
+
+                ClientScript.RegisterStartupScript(this.GetType(), "loginAlert", script, true);
+                return;
+            }
+            else
+            {
+                string firstname = Session["firstname"].ToString();
+                string lastname = Session["lastname"].ToString();
+                lblname.Text = "Welcome back " + firstname + " " + lastname;
+            }
+
+        }
+    }
+}
